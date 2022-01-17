f9737d04199a73bc3a34f5e27caa216c9e255759
Bishal Gautam <gautambishal717@gmail.com>
2022-01-17 17:29:23 +0545

    eshop 1st commit

---
+++ src/Web/Controllers/BankDetailsController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microsoft.eShopWeb.Web.Controllers;
+public class BankDetailsController : Controller
+{
+    // GET: BankDetailsController
+    public ActionResult PayAmount()
+    {
+        return View();
+    }
+
+    // GET: BankDetailsController/Details/5
+    public ActionResult Details(int id)
+    {
+        return View();
+    }
+
+    // GET: BankDetailsController/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: BankDetailsController/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: BankDetailsController/Edit/5
+    public ActionResult Edit(int id)
+    {
+        return View();
+    }
+
+    // POST: BankDetailsController/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: BankDetailsController/Delete/5
+    public ActionResult Delete(int id)
+    {
+        return View();
+    }
+
+    // POST: BankDetailsController/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Web/Controllers/Proceed.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microsoft.eShopWeb.Web.Controllers;
+public class Proceed : Controller
+{
+    // GET: Proceed
+    public ActionResult ProceedActions()
+    {
+        return View();
+    }
+
+    // GET: Proceed/Details/5
+    public ActionResult Details(int id)
+    {
+        return View();
+    }
+
+    // GET: Proceed/Create
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    // POST: Proceed/Create
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Create(IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: Proceed/Edit/5
+    public ActionResult Edit(int id)
+    {
+        return View();
+    }
+
+    // POST: Proceed/Edit/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Edit(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+
+    // GET: Proceed/Delete/5
+    public ActionResult Delete(int id)
+    {
+        return View();
+    }
+
+    // POST: Proceed/Delete/5
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public ActionResult Delete(int id, IFormCollection collection)
+    {
+        try
+        {
+            return RedirectToAction(nameof(Index));
+        }
+        catch
+        {
+            return View();
+        }
+    }
+}
